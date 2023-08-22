3e72d66b9201a54edf0d8a8e9dc0784f9df58d77
Ahmed Zayed <azayed325@gmail.com>
2023-08-22 02:11:13 +0200

    EDA and Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_msg_detail(error, error_detail:sys):
     _, _, tb = error_detail.exc_info()
@@ -15,5 +15,12 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
-        
+    
+    
+if __name__ == '__main__':
+    try:
+        x = 1/0
+    except Exception as e:
+        logging.error(e)
+        raise CustomException(e, sys)
         
--- src/logger.py
+++ src/logger.py
@@ -3,13 +3,18 @@ import os
 from datetime import datetime
 
 
-LOG_FILE = f'{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log'
+LOG_FILE = f'{datetime.now().strftime("%m_%d_%Y")}.log'
 logs_path = os.path.join(os.getcwd(), 'logs')
 os.makedirs(os.path.dirname(logs_path), exist_ok=True)
-
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(filename=LOG_FILE_PATH,
                     level=logging.INFO,
                     format='[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s')
+
+if __name__ == '__main__':
+    logging.info('This is a info message')
+    logging.warning('This is a warning message')
+    logging.error('This is a error message')
+    logging.critical('This is a critical message')
 
